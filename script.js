b037fe81bfdbef3a299a7e507d16d84c6ae9f0ac
Cassandra <cassandra.moore1516@gmail.com>
2025-04-15 18:05:24 -0500

    Creating online-frontend-course

---
+++ script.js
@@ -0,0 +1,6 @@
+const quizButton = document.querySelector('#interactive-quiz button');
+if (quizButton) {
+    quizButton.addEventListener('click', function() {
+        alert('Basic quiz functionality will be implemented in a future version!');
+    });
+}
